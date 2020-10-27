b778c0af893e5f47fef5044df9119e673a3ba22e
TGilgameshT <kravenstark@gmail.com>
2020-10-27 20:36:07 -0300

    Ajuste para Teste.

--- gravarusuario.php
+++ gravarusuario.php
@@ -1,12 +1,13 @@
 <?php
   $nome = $_POST['nome'];
   $email = $_POST['email'];
+  $pass = $_POST['senha'];
   $datnas = $_POST['dataNasc'];
   $endereco = $_POST['endereco'];
   $cidade = $_POST['cidade'];
   $estado = $_POST['estado'];
   $cep = $_POST['cep'];
-  $sql = "insert into dbo.USUARIO values ('$nome','$email','$datnas', '$endereco', '$cidade','$estado', $cep)";
+  $sql = "insert into dbo.USUARIO values ('$nome','$email',$pass,'$datnas','$endereco','$cidade','$estado',$cep)";
   $conexao = mssql_connect('castelo-server.database.windows.net','CasteloAdmin','AdminCastelo123','DB_CASTELO');
   if($conexao!=null)
   {

cc997ed70d94519e7d7bb8501ea656e388f362d8
TGilgameshT <kravenstark@gmail.com>
2020-10-28 02:30:01 -0300

    Conexão com o banco.

--- gravarcrianca.php
+++ gravarcrianca.php
@@ -6,6 +6,10 @@
   $cidade = $_POST['cidade'];
   $estado = $_POST['estado'];
   $cep = $_POST['cep'];
+
+  if($sexoM == "M"){​​​​​$sexo = "0"}​​​​​
+  if($sexoF == "F"){​​​​​$sexo = "1"}​​​​​
+
   $sql = "insert into dbo.CRIANCA values ('$nome','$email','$datnas', '$endereco', '$cidade','$estado', $cep)";
   $conexao = mssql_connect('castelo-server.database.windows.net','CasteloAdmin','AdminCastelo123','DB_CASTELO');
   if($conexao!=null)
--- gravarusuario.php
+++ gravarusuario.php
@@ -1,27 +1,33 @@
 <?php
-  $erro = null;
+  include "model/mssql.class.php";
+  $dbSite= new db("castelo-server.database.windows.net", "CasteloAdmin", "AdminCastelo123", "DB_CASTELO");
+  echo "ok";
   $nome = $_POST['nome'];
   $email = $_POST['email'];
-  $pass = $_POST['senha'];
+  $pass = md5($_POST['senha']);
   $datnas = $_POST['dataNasc'];
   $endereco = $_POST['endereco'];
   $cidade = $_POST['cidade'];
   $estado = $_POST['estado'];
   $cep = $_POST['cep'];
-  try {
-    $conexao = mssql_connect('castelo-server.database.windows.net','CasteloAdmin','AdminCastelo123','DB_CASTELO');  
-    $sql = "insert into dbo.USUARIO values ('$nome','$email',$pass,'$datnas','$endereco','$cidade','$estado',$cep)";
-    if($conexao!=null)
-    {
-        mssql_query($conexao,$sql);
-        mssql_close($conexao);
-    }
-    else {
-        Echo 'Falhou a conexão';
-    }
-  } catch (\Throwable $e) {
-        echo 'Exceção capturada: ',  $e->getMessage(), "\n";
-  }
+  $nmcri = $_POST['nomeCrianca'];
+  $sexoM = $_POST['sexoM'];
+  $sexoF = $_POST['sexoF'];
+  $sexo = NULL;
+  if($sexoM == "M"){​​​​​$sexo = "0"}​​​​​
+  else if($sexoF == "F"){​​​​​$sexo = "1"}
+  $datnascri = $_POST['dataNascCrianca'];
+  $avescola = $_POST['escola'];
+  $avjogos = $_POST['jogos'];
+  $avmusicas = $_POST['musicas'];
+  $avaniver = $_POST['aniversario'];
 
-  header('location: cadastro.html');
+  $query = $dbSite->query("insert into dbo.USUARIO values ('".$nome."','".$email."','".$pass."','".$datnas."','".$endereco."','".$cidade."','".$estado."','".$cep."','".$nmcri."','".$sexo."','".$datnascri."','".$avescola."','".$avjogos."','".$avmusicas."','".$avaniver."')");
+  echo $dbSite->query("insert into dbo.USUARIO values ('".$nome."','".$email."','".$pass."','".$datnas."','".$endereco."','".$cidade."','".$estado."','".$cep."','".$nmcri."','".$sexo."','".$datnascri."','".$avescola."','".$avjogos."','".$avmusicas."','".$avaniver."')");
+  if($query){
+    //header('location: index.html');
+  }else{
+    echo json_encode(false);
+  }
+  
 ?>

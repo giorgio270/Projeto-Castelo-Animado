5472264cfe34cececd3b874f4601f168bfacd052
TGilgameshT <kravenstark@gmail.com>
2020-10-28 17:23:19 -0300

    Cadastro e ajustes.

--- gravarusuario.php
+++ gravarusuario.php
@@ -1,7 +1,7 @@
 <?php
   include "model/mssql.class.php";
   $dbSite= new db("castelo-server.database.windows.net", "CasteloAdmin", "AdminCastelo123", "DB_CASTELO");
-  echo "ok";
+  
   $nome = $_POST['nome'];
   $email = $_POST['email'];
   $pass = md5($_POST['senha']);
@@ -11,11 +11,7 @@
   $estado = $_POST['estado'];
   $cep = $_POST['cep'];
   $nmcri = $_POST['nomeCrianca'];
-  $sexoM = $_POST['sexoM'];
-  $sexoF = $_POST['sexoF'];
-  $sexo = NULL;
-  if($sexoM == "M"){​​​​​$sexo = "0"}​​​​​
-  else if($sexoF == "F"){​​​​​$sexo = "1"}
+  $sexo = $_POST['sexo'];
   $datnascri = $_POST['dataNascCrianca'];
   $avescola = $_POST['escola'];
   $avjogos = $_POST['jogos'];
@@ -23,9 +19,9 @@
   $avaniver = $_POST['aniversario'];
 
   $query = $dbSite->query("insert into dbo.USUARIO values ('".$nome."','".$email."','".$pass."','".$datnas."','".$endereco."','".$cidade."','".$estado."','".$cep."','".$nmcri."','".$sexo."','".$datnascri."','".$avescola."','".$avjogos."','".$avmusicas."','".$avaniver."')");
-  echo $dbSite->query("insert into dbo.USUARIO values ('".$nome."','".$email."','".$pass."','".$datnas."','".$endereco."','".$cidade."','".$estado."','".$cep."','".$nmcri."','".$sexo."','".$datnascri."','".$avescola."','".$avjogos."','".$avmusicas."','".$avaniver."')");
+ 
   if($query){
-    //header('location: index.html');
+    header('location: index.html');
   }else{
     echo json_encode(false);
   }
